99a1e08cb47bed085589858191663b0166080341
Diego Alejandro Panqueva Benitez <daerb9011@gmail.com>
2025-03-17 18:05:44 +0100

    Initial commit

---
+++ src/main/java/com/bcnc/inditex/pvp/mcsv/PvpMcsvApplication.java
@@ -0,0 +1,13 @@
+package com.bcnc.inditex.pvp.mcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvpMcsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvpMcsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcnc/inditex/pvp/mcsv/PvpMcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnc.inditex.pvp.mcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvpMcsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
