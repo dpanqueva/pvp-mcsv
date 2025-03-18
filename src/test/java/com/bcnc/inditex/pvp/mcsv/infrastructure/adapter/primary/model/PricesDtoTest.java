ec45bbc3370f970a19acaba977aebef3640feb46
Diego Alejandro Panqueva Benitez <daerb9011@gmail.com>
2025-03-18 11:04:50 +0100

    Feat: Fix sonar alerts

--- src/test/java/com/bcnc/inditex/pvp/mcsv/infrastructure/adapter/primary/model/PricesDtoTest.java
+++ src/test/java/com/bcnc/inditex/pvp/mcsv/infrastructure/adapter/primary/model/PricesDtoTest.java
@@ -23,31 +23,4 @@ class PricesDtoTest {
         assertEquals(5.0f, pricesDto.getPricePvp());
         assertEquals("EUR", pricesDto.getCurr());
     }
-
-
-
-    @Test
-    void testAllArgsConstructor() {
-        PricesDto pricesDto = new PricesDto(
-                1L
-                ,2L
-                ,LocalDateTime.now()
-                ,LocalDateTime.now()
-                ,"priceList"
-                ,3L
-                ,4L
-                ,5.0f
-                ,"EUR"
-                );
-
-        assertEquals(1L, pricesDto.getIdPrice());
-        assertEquals(2L, pricesDto.getBrandId());
-        assertNotNull(pricesDto.getStartDate());
-        assertNotNull(pricesDto.getEndDate());
-        assertEquals("priceList", pricesDto.getPriceList());
-        assertEquals(3L, pricesDto.getProductId());
-        assertEquals(4L, pricesDto.getPriority());
-        assertEquals(5.0f, pricesDto.getPricePvp());
-        assertEquals("EUR", pricesDto.getCurr());
-    }
 }
